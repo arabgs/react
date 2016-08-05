9a288ef6de62fc0367c9e2507b00a87ddf3cd72f
arabgs <ara.bgs@gmail.com>
2016-08-04 21:37:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react';
+
+class Hello extends React Component {
+	render(){
+
+return (
+
+<p>Hello world</p>
+);
+
+	}
+
+
+}
+
+const app =document getElementById('container');
+
+ReactDOM render(<Hello />,app);
